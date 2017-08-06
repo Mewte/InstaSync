805b6d6c0aac0ca84c5fb46cd7f6c5a85bfbf106
Mewte <lindner4147@yahoo.com>
2017-08-05 20:15:11 -0400

    Updated sql schema as it was missing critical tables
    sql dump was from a while back, it was missing the new sessions table
    and the additional users fields

--- schema/schema.sql
+++ schema/schema.sql
@@ -1,74 +1,105 @@
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- phpMyAdmin SQL Dump
+-- version 4.7.2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 06, 2017 at 02:14 AM
+-- Server version: 5.6.35
+-- PHP Version: 7.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+--
+-- Database: `instasynch`
+--
 
+-- --------------------------------------------------------
 
-CREATE TABLE IF NOT EXISTS `bans` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+--
+-- Table structure for table `bans`
+--
+
+CREATE TABLE `bans` (
+  `id` int(11) NOT NULL,
   `ip` varchar(45) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `room_name` varchar(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `user_id` int(11) DEFAULT NULL,
   `username` varchar(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `loggedin` tinyint(1) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `room_name` (`room_name`,`user_id`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10928 ;
-
-CREATE TABLE IF NOT EXISTS `friends_list` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userA` int(11) NOT NULL,
-  `userB` int(11) NOT NULL,
-  `sentBy` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `userA` (`userA`,`userB`),
-  KEY `sentBy` (`sentBy`),
-  KEY `userB` (`userB`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10800 ;
-
-CREATE TABLE IF NOT EXISTS `friend_requests` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `userA` int(11) NOT NULL,
-  `userB` int(11) NOT NULL,
-  `sentBy` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `userA` (`userA`,`userB`),
-  KEY `sentBy` (`sentBy`),
-  KEY `userB` (`userB`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=15072 ;
-
-CREATE TABLE IF NOT EXISTS `ips` (
-  `ip` varchar(45) NOT NULL,
-  `count` int(3) NOT NULL,
-  PRIMARY KEY (`ip`)
+  `loggedin` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `mods` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friends_list`
+--
+
+CREATE TABLE `friends_list` (
+  `id` int(11) NOT NULL,
+  `userA` int(11) NOT NULL,
+  `userB` int(11) NOT NULL,
+  `sentBy` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `friend_requests`
+--
+
+CREATE TABLE `friend_requests` (
+  `id` int(11) NOT NULL,
+  `userA` int(11) NOT NULL,
+  `userB` int(11) NOT NULL,
+  `sentBy` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ips`
+--
+
+CREATE TABLE `ips` (
+  `ip` varchar(45) NOT NULL,
+  `count` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `mods`
+--
+
+CREATE TABLE `mods` (
+  `id` int(11) NOT NULL,
   `room_name` varchar(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
   `username` varchar(16) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
-  `permissions` int(11) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `room` (`room_name`,`username`),
-  KEY `user_id` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7528 ;
+  `permissions` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `resets` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resets`
+--
+
+CREATE TABLE `resets` (
+  `id` int(11) NOT NULL,
   `token` varchar(73) NOT NULL,
   `user_id` int(11) NOT NULL,
   `time` int(11) NOT NULL,
-  `ip` varchar(45) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `token` (`token`),
-  KEY `user_id` (`user_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1535 ;
+  `ip` varchar(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE IF NOT EXISTS `rooms` (
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `rooms`
+--
+
+CREATE TABLE `rooms` (
   `room_id` int(11) NOT NULL,
   `room_name` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
   `description` varchar(160) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -78,13 +109,30 @@ CREATE TABLE IF NOT EXISTS `rooms` (
   `title` varchar(64) COLLATE utf8_unicode_ci DEFAULT NULL,
   `listing` enum('public','private') COLLATE utf8_unicode_ci NOT NULL DEFAULT 'public',
   `info` varchar(2048) COLLATE utf8_unicode_ci NOT NULL,
-  `NSFW` int(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`room_id`),
-  UNIQUE KEY `roomname` (`room_name`)
+  `NSFW` int(1) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
+CREATE TABLE `sessions` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `cookie` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `username` varchar(16) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
   `username` varchar(16) COLLATE utf8_unicode_ci NOT NULL,
   `hashpw` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
   `cookie` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
@@ -93,36 +141,173 @@ CREATE TABLE IF NOT EXISTS `users` (
   `bio` varchar(1024) COLLATE utf8_unicode_ci NOT NULL DEFAULT 'No Bio',
   `social` tinyint(1) NOT NULL DEFAULT '1',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=71526 ;
+  `registered_ip` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
+  `last_login` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `bans`
+--
+ALTER TABLE `bans`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `room_name` (`room_name`,`user_id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `friends_list`
+--
+ALTER TABLE `friends_list`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `userA` (`userA`,`userB`),
+  ADD KEY `sentBy` (`sentBy`),
+  ADD KEY `userB` (`userB`);
+
+--
+-- Indexes for table `friend_requests`
+--
+ALTER TABLE `friend_requests`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `userA` (`userA`,`userB`),
+  ADD KEY `sentBy` (`sentBy`),
+  ADD KEY `userB` (`userB`);
+
+--
+-- Indexes for table `ips`
+--
+ALTER TABLE `ips`
+  ADD PRIMARY KEY (`ip`);
+
+--
+-- Indexes for table `mods`
+--
+ALTER TABLE `mods`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `room` (`room_name`,`username`),
+  ADD KEY `user_id` (`username`);
+
+--
+-- Indexes for table `resets`
+--
+ALTER TABLE `resets`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `token` (`token`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `rooms`
+--
+ALTER TABLE `rooms`
+  ADD PRIMARY KEY (`room_id`),
+  ADD UNIQUE KEY `roomname` (`room_name`);
+
+--
+-- Indexes for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`),
+  ADD KEY `username` (`username`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bans`
+--
+ALTER TABLE `bans`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `friends_list`
+--
+ALTER TABLE `friends_list`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `friend_requests`
+--
+ALTER TABLE `friend_requests`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `mods`
+--
+ALTER TABLE `mods`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `resets`
+--
+ALTER TABLE `resets`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `sessions`
+--
+ALTER TABLE `sessions`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bans`
+--
 ALTER TABLE `bans`
   ADD CONSTRAINT `bans_ibfk_1` FOREIGN KEY (`room_name`) REFERENCES `rooms` (`room_name`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `bans_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `friends_list`
+--
 ALTER TABLE `friends_list`
   ADD CONSTRAINT `friends_list_ibfk_3` FOREIGN KEY (`sentBy`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `friends_list_ibfk_4` FOREIGN KEY (`userA`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `friends_list_ibfk_5` FOREIGN KEY (`userB`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `friend_requests`
+--
 ALTER TABLE `friend_requests`
   ADD CONSTRAINT `friend_requests_ibfk_1` FOREIGN KEY (`userA`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `friend_requests_ibfk_2` FOREIGN KEY (`userB`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `friend_requests_ibfk_3` FOREIGN KEY (`sentBy`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `mods`
+--
 ALTER TABLE `mods`
   ADD CONSTRAINT `mods_ibfk_3` FOREIGN KEY (`room_name`) REFERENCES `rooms` (`room_name`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `mods_ibfk_4` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `resets`
+--
 ALTER TABLE `resets`
   ADD CONSTRAINT `resets_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
+--
+-- Constraints for table `rooms`
+--
 ALTER TABLE `rooms`
   ADD CONSTRAINT `rooms_ibfk_1` FOREIGN KEY (`room_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `rooms_ibfk_2` FOREIGN KEY (`room_name`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+--
+-- Constraints for table `sessions`
+--
+ALTER TABLE `sessions`
+  ADD CONSTRAINT `sessions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `sessions_ibfk_2` FOREIGN KEY (`username`) REFERENCES `users` (`username`) ON DELETE CASCADE ON UPDATE CASCADE;
